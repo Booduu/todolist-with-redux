c586af6d03cccd49c64f8d0086e66486b6f12509
Mathias Payen <mathias.payen@gmail.com>
2020-09-18 11:14:04 +0200

    change color background

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,46 +1,4 @@
-// import * as actions from './actions'
-
-
-// export const todoReducer = (state, action) => {
-//     switch(action.type) {
-//         case actions.ADD_TODO: {
-//             return {
-//                 ...state,
-//                 todos: [ ...state.todos, actions.todo]
-//             }
-//         }
-//         case actions.DELETE_TODO: {
-//             return {
-//                 ...state, 
-//                 todos: state.todo.filter( (t, index) => i !== action.index )
-//             }
-//         }
-//         case actions.SET_FILTER: {
-//             return {
-//                 ...state,
-//                 filter: action.filter
-//             }
-//         }
-//         case actions.TOGGLE_TODO: {
-//             return {
-//                 ...state,
-//                 todo: state.todo.map( (t, i) => {
-//                     if( i === action.index ) {
-//                         t.done = !t.done
-//                     }
-//                     return t;
-//                 })
-//             }
-//         }
-//         default: {
-//             return state
-//         }
-//     }
-// }
-
-
 import * as actions from './actions';
-
 
 export const todos = (state = [], action) => {
     switch (action.type) {
@@ -70,12 +28,3 @@ export const filter = (state = actions.visibilityFilters.SHOW_ALL, action) => {
         }
     }
 }
-
-
-// export const todoReducer = combineReducers({ todos, filter })
-// export const todosReducer = (state, action) => {
-//      return {
-//          todos: todoReducer(state.todos, action),
-//          filter: filterReducer(state.filter, action)
-//      }
-// }
